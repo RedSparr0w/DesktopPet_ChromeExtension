b42ce7a3f14f7a4e9f8470a6aa5a1cb8a8a80b50
Danial <RedSparr0w@users.noreply.github.com>
2019-02-08 10:12:17 +1300

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ chrome.browserAction.onClicked.addListener(function(tab) {
     var id = prompt("Enter ID of Pet:", 5);
     if (!id) return;
     chrome.tabs.executeScript({
-      code: `new DesktopPet().start_esheep("http://esheep.petrucci.ch/script/pettester.php?id=${id}");`
+      code: `new DesktopPet().start_esheep("https://esheep.redsparr0w.com/${id}");`
     });
   });
 });
